53875d1ed3bc7f5c64f9decfeb561b1f85929c97
SARVESHWARAN K <70128993+Sarveshwaran20@users.noreply.github.com>
2024-11-04 17:05:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// const container = document.getElementById('star-container');
+// function createShootingStar() {
+//   const star = document.createElement('div');
+//   star.classList.add('shooting-star');
+//   // Set a random position for each shooting star
+//   console.log(3, "createShootingStar");
+//   const startPosX = Math.random() * window.innerWidth;
+//   const startPosY = Math.random() * window.innerHeight;
+//   star.style.left = `${startPosX}px`;
+//   star.style.bottom = `${startPosY}px`;
+//   container.appendChild(star);
+//   setTimeout(() => {
+//     container.removeChild(star);
+//   }, 2900); // matches animation duration
+// }
+
+// // Generate a shooting star every 500 ms
+// setInterval(createShootingStar, 2000);
